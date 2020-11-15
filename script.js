bf31d071008c4c9012ec86e811137bd21ad75477
JohanFre <70636307+JohanFre@users.noreply.github.com>
2020-11-15 19:02:31 +0100

    patch 1.0

---
+++ script.js
@@ -0,0 +1,53 @@
+// Variables and fruit array declaration.
+const websiteHost = document.querySelector(".hostName");
+const jokeBtnEl = document.querySelector(".getRandomJoke");
+const randomJoke = document.querySelector(".randomJoke");
+const fruitListEl = document.querySelectorAll(".all-the-fruits");
+const sortFruitsEl = document.querySelector(".sortFruits");
+const jokeUrl = "http://api.icndb.com/jokes/random";
+const fruitsEl = [
+  "Banana",
+  "Pineapple",
+  "Apple",
+  "Pear",
+  "Orange",
+  "Mango",
+  "Apricot",
+  "Cherry",
+  "Fig",
+  "Grape",
+  "Plum",
+];
+
+// Event listener for hostName button.
+websiteHost.addEventListener("click", () => {
+  alert("Your href is: " + location.href);
+});
+
+// Console logs different global location object.
+console.log(location.origin);
+console.log(location.pathname);
+console.log(location.hostname);
+
+// Event listener for fetching jokeAPI and displaying the joke.
+jokeBtnEl.addEventListener("click", () => {
+  fetch(jokeUrl)
+    .then((response) => response.json())
+    .then((data) => (randomJoke.innerHTML = data.value.joke));
+});
+
+// Creates listitems from fruit array.
+var ul = document.createElement("ul");
+document.querySelectorAll(".all-the-fruits")[0].appendChild(ul);
+
+for (var i = 0; i < fruitsEl.length; i++) {
+  let li = document.createElement("li");
+  li.innerHTML = fruitsEl[i];
+  ul.appendChild(li);
+}
+
+// Event listener for fruit filterbutton / console logs short named fruits.
+sortFruitsEl.addEventListener("click", () => {
+  let filterEl = fruitsEl.filter((word) => word.length <= 4);
+  console.log(filterEl);
+});
